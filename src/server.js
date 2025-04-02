e30bd79ea6f8a41875f19b7ffb3a8a94c211a06b
Nestor Júnior <nestorjunior@MacBook-Pro-de-Nestor.local>
2025-04-02 09:38:04 -0300

    feat: create post req body

--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-app.get('/users/:id/:name', (request, response)=> {
-    const {id, name} = request.params
-    response.send(`User ID: ${id}. User: ${name}`)
+// app.get('/', (request, response)=> {
+//     response.send('Hi!')
+// })
+
+// app.get('/users/:id/:name', (request, response)=> {
+//     const {id, name} = request.params
+//     response.send(`User ID: ${id}. User: ${name}`)
+// })
+
+// app.get('/products', (request, response)=> {
+//     const {page, limit} = request.query
+//     response.send(`Page: ${page}. Limit: ${limit}`)
+// })
+
+app.post('/users', (req, res)=> {
+    const {name, email, pass} = req.body
+    res.send(`POST: ${name} + ${email} + ${pass}`)
 })
-
-app.get('/products', (request, response)=> {
-    const {page, limit} = request.query
-    response.send(`Page: ${page}. Limit: ${limit}`)
-})
-
 
 const PORT = 3333
 app.listen(PORT, ()=> console.log(`Server is running on Port ${PORT}`))
