fd1af8f8fc44e2588c555a4de22c7419ac5832fc
Nestor Júnior <nestorjunior@MacBook-Pro-de-Nestor.local>
2025-04-02 07:37:54 -0300

    feat: params request simple route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,16 @@ const express = require('express')
 
 const app = express()
 
+app.get('/users/:id/:name', (request, response)=> {
+    const {id, name} = request.params
+    
+    response.send(`
+        User ID: ${id}.
+        User: ${name}
+    `)
+})
+
+
 const PORT = 3333
 
 app.listen(PORT, ()=> console.log(`Server is running on Port ${PORT}`))
